b131d60045dfc3ac74f8c9de3b4479c6c1970d10
2356701 <nthnfortier@gmail.com>
2025-02-25 13:40:32 -0500

    sweraftd

--- Assets/Script/SousmarinAvance.cs
+++ Assets/Script/SousmarinAvance.cs
@@ -7,17 +7,28 @@ public class SousmarinAvance : MonoBehaviour
 {
     [SerializeField] private float _vitessePromenade = 5f;
     [SerializeField] private float _vitesseVerticale = 3f;
+    [SerializeField] private float _boostPromenade = 25f;
+    [SerializeField] private float _boostVerticale = 20f;
+
+    private float _vitesseActuellePromenade;
+    private float _vitesseActuelleVerticale;
+
     private Rigidbody _rb;
     private Vector3 directionInput;
       private Animator _animator;
 
     void Start()
     {
-        _rb = GetComponent<Rigidbody>(); 
-          _animator = GetComponent<Animator>(); 
+        _rb = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
+
+       
+        _vitesseActuellePromenade = _vitessePromenade;
+        _vitesseActuelleVerticale = _vitesseVerticale;
     }
 
- 
+
+
     void OnAvance(InputValue directionBase)
     {
         Vector2 input = directionBase.Get<Vector2>();
@@ -57,9 +68,29 @@ public class SousmarinAvance : MonoBehaviour
         }
     }
 
-    void FixedUpdate()
+    void OnAccelere(InputValue etatBouton)
     {
-        Vector3 mouvement = directionInput * Time.fixedDeltaTime;
+        if (etatBouton.isPressed)
+        {
+            Debug.Log("presse");
+            _vitesseActuellePromenade = _boostPromenade;
+            _vitesseActuelleVerticale = _boostVerticale;
+        }
+        else
+        {
+            Debug.Log("lache");
+            _vitesseActuellePromenade = _vitessePromenade;
+            _vitesseActuelleVerticale = _vitesseVerticale;
+        }
+    }
+
+
+
+
+    void Update()
+    {
+        Vector3 mouvement = new Vector3(0, directionInput.y * _vitesseActuelleVerticale, directionInput.z * _vitesseActuellePromenade) * Time.deltaTime;
         _rb.MovePosition(_rb.position + mouvement);
     }
+
 }
